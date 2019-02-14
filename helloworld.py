9e03c362388f05bd8955b227639bf45d61c26fbd
Jessica <seathje@wgc.school.nz>
2019-02-15 10:03:38 +1300

    added background colours and fonts

--- helloworld.py
+++ helloworld.py
@@ -5,7 +5,15 @@ root.title("Hello World!")
 root.geometry("600x200")
 
 my_label = tkinter.Label(root)
-my_label.config(text="Hello!", fg="green")
+my_label.config(text="Hello!", fg="pink", bg="pink", font=("Wide latin", "50", "italic"))
+my_label.grid()
+
+my_label = tkinter.Label(root)
+my_label.config(text="Hello there!", fg="green", bg="pink", font=("Wide latin", "50", "italic"))
+my_label.grid()
+
+my_label = tkinter.Label(root)
+my_label.config(text="Hey you!", fg="orange", bg="yellow", font=("Wide latin", "50", "italic"))
 my_label.grid()
 
 root.mainloop()
